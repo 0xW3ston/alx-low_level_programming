1efacd1e5adca9d8f8ad2c75410aa6f2f947fce8
Youssef El Idrissi <professionalidrissi@gmail.com>
2023-09-13 02:29:57 +0100

    added task 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int iTeration = 0;
+	unsigned long int sum = 0, new, n1 = 1, n2 = 2;
+
+	while (iTeration < 33)
+	{
+		iTeration++;
+		
+		new = n1 + n2;
+		if (n1 < 4000000 && !(n1 % 2))
+		{
+			sum = sum + n1;
+		}
+		n1 = n2;
+		n2 = new;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+}
