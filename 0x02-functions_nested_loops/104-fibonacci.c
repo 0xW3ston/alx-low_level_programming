d055779abbad67dd83f608be566f5d7435d5329c
Youssef El Idrissi <professionalidrissi@gmail.com>
2023-09-13 02:54:53 +0100

    fixed task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -25,15 +25,15 @@ int main(void)
 	iFFnsh1 = (iFibFnsh / nLong);
 	iFFnsh2 = (iFibFnsh % nLong);
 
-	while (iSection < 99)
+	while (iSection < 98)
 	{
-		iSection++;
 		printf(", %lu", iFFnsh1 + (iFFnsh2 / nLong));
 		printf("%lu", iFFnsh2 % nLong);
 		iFFnsh1 = iFFnsh1 + iFStr1;
 		iFStr1 = iFFnsh1 - iFStr1;
 		iFFnsh2 = iFFnsh2 + iFStr2;
 		iFStr2 = iFFnsh2 - iFStr2;
+		iSection++;
 	}
 	putchar('\n');
 
