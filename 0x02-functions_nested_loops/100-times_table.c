d829f7cfef43cfe56bce96702d3c0036b7a98305
Youssef El Idrissi <professionalidrissi@gmail.com>
2023-09-13 03:12:28 +0100

    simplified task 12 for 40 lines for betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,6 +8,8 @@
  */
 void print_times_table(int n)
 {
+	void compare_then_print(int);
+
 	int num = 0;
 	int power = 0;
 	int result = 0;
@@ -23,29 +25,9 @@ void print_times_table(int n)
 				{
 					_putchar('0');
 				}
-				if (result < 10 && power != 0)
+				if (power != 0)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + result);
-				}
-				if (result > 9 && result < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + (result / 10));
-					_putchar('0' + (result % 10));
-				}
-				if (result > 99)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar('0' + (result / 100));
-					_putchar('0' + ((result / 10) % 10));
-					_putchar('0' + (result % 10));
+					compare_then_print(result);
 				}
 				power++;
 			}
@@ -53,5 +35,29 @@ void print_times_table(int n)
 			num++;
 			_putchar('\n');
 		}
+	}
+}
+
+void compare_then_print(int result)
+{
+	_putchar(',');
+	_putchar(' ');
+	if (result < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar('0' + result);
+	}
+	if (result > 9 && result < 100)
+	{
+		_putchar(' ');
+		_putchar('0' + (result / 10));
+		_putchar('0' + (result % 10));
+	}
+	if (result > 99)
+	{
+		_putchar('0' + (result / 100));
+		_putchar('0' + ((result / 10) % 10));
+		_putchar('0' + (result % 10));
 	}
 }
