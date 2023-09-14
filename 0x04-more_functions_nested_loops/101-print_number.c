272606288c6d642b1c0d63a31ae222dd158fcd69
Youssef El Idrissi <professionalidrissi@gmail.com>
2023-09-14 23:35:45 +0100

    fixed 101 and added 100

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_number - prints integers
@@ -7,10 +8,30 @@
  */
 void print_number(int n)
 {
-	int number = n;
+	unsigned int number;
+	unsigned int c;
+	unsigned int total;
 
-	if (number < 0)
-	{
+	if (n < 0)
+	{	number = n * -1;
 		_putchar('-');
+	}
+	else
+	{
+		number = n;
+	}
+
+	c = 1;
+	total = number;
+
+	while (total > 9)
+	{
+		total = total / 10;
+		c *= 10;
+	}
+
+	for (c = c; c >= 1; c /= 10)
+	{
+		_putchar('0' + ((number / c) % 10));
 	}
 }
