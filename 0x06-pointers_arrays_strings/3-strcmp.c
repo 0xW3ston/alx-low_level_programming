f4633e70358b8cff05cdeb7743958fb64e3882f9
Youssef El Idrissi <professionalidrissi@gmail.com>
2023-09-22 00:51:16 +0100

    added task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings and returns an int
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: result of the comparaison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int res;
+
+	res = strcmp(s1, s2);
+
+	return (res);
+}
