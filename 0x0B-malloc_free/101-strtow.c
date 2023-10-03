221aab6bce99f8c65b0e8a4ef2d361e811b012d9
Youssef El Idrissi <professionalidrissi@gmail.com>
2023-10-03 18:47:50 +0100

    added task 6 (avncd)

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,90 @@
+#include "main.h"
+
+/**
+ * strtow - splits a string => words.
+ * @str: string ptr
+ *
+ *
+ * Return: array ptr if successful
+ * else NULL if not successful
+*/
+char **strtow(char *str)
+{
+	int _wc(char *str);
+	char **m, *tab;
+	int i, j, length = 0, w_len, charr = 0, start, end;
+
+	j = 0, charr = 0, length = 0;
+
+	while (str[length])
+	{
+		length++;
+	}
+
+	w_len = _wc(str);
+
+	if (w_len == 0)
+	{
+		return (NULL);
+	}
+
+	m = (char **) malloc(sizeof(char *) * (w_len + 1));
+	if (m == NULL)
+		return (NULL);
+
+	for (i = 0; i <= length; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (charr)
+			{
+				end = i;
+				tab = (char *) malloc(sizeof(char) * (charr + 1));
+				if (tab == NULL)
+					return (NULL);
+				while (start < end)
+					*tab++ = str[start++];
+				*tab = '\0';
+				m[j] = tab - charr;
+				j++;
+				charr = 0;
+			}
+		}
+		else if (charr++ == 0)
+			start = i;
+	}
+
+	m[j] = NULL;
+
+	return (m);
+}
+
+/**
+ * _wc - word counter
+ * @str: str to test
+ *
+ * Return: always returns int (amount of words)
+ */
+
+int _wc(char *str)
+{
+	int count, i, total;
+
+	count = 0;
+	total = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			count = 0;
+		}
+		else if (count == 0)
+		{
+			count = 1;
+			total++;
+		}
+	}
+
+	return (total);
+}
