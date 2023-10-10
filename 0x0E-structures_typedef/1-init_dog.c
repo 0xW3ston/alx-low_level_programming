cfd9700863e6e16fc9e6c72e3003a4fe270ad22e
weston <professionalidrissi@gmail.com>
2023-10-10 19:23:15 +0100

    Added task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+/**
+ * init_dog - initializes a dog struct with specific
+ * values.
+ *
+ * @d: dog struct object
+ * @name: name (of the dog)
+ * @age: age (of the dog)
+ * @owner: owner's name
+ *
+ * Return: Nothing
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+
+	(*d).age = age;
+	(*d).name = name;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+Learned how to use structs to make my own
+User-Defined data structures (such as dog).
+I learned about initialization of the members/attributes.
+and also how to use typedef (which in a way declares a new datatype)
